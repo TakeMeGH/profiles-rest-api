73df052bb6fd199d33fb5502477cf9259dc57eb2
Abdul Rafi <abdulrafi907@gmail.com>
2021-12-28 14:23:37 +0700

    Add my first API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -36,6 +36,8 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    object = UserProfileManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, requset, format = None):
+
+        an_apiview = [
+            "Test 123",
+            "Test 321",
+            "Test 213",
+        ]
+
+        return Response({"Message" : "Halo Dunia", "APIView" : an_apiview})
