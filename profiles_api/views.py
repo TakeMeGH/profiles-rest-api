15255304cc6dc53a4b65f6e1b9944d5d121c40fd
Abdul Rafi <abdulrafi907@gmail.com>
2021-12-28 17:03:39 +0700

    Added put, patch, delete to APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
 
-    def get(self, requset, format = None):
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format = None):
 
         an_apiview = [
             "Test 123",
@@ -13,3 +18,28 @@ class HelloApiView(APIView):
         ]
 
         return Response({"Message" : "Halo Dunia", "APIView" : an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            msg = f"Hello {name}"
+            return Response({
+                "Message" : msg,
+                "Email" : serializer.validated_data.get("email")}
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({"Method" : "PUT"})
+
+    def patch(self, request, pk=None):
+        return Response({"Method" : "Patch"})
+
+    def delete(self, request, pk=None):
+        return Response({"Method" : "Delete"})
