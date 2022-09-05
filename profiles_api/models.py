6e2032880ff4f96cbc1f1622b5e3b493d4336d92
Abdul Rafi <abdulrafi907@gmail.com>
2022-09-05 19:24:10 +0700

    tambah fitur feed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.conf import settings
 
 class UserProfileManager(BaseUserManager):
     
@@ -48,3 +49,14 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    status_tags = models.CharField(max_length=25)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Text : {self.status_text} \n Tags : {self.status_tags}"
