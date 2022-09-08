254524ba1d2f52fb74542fc221903b3bf2c968f5
Abdul Rafi <abdulrafi907@gmail.com>
2022-09-08 14:05:08 +0700

    tambah postgress

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -132,3 +132,7 @@ AUTH_USER_MODEL = 'profiles_api.UserProfile'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
